4fe1846e2c68ef7a1ca1e0eedba3aef1f0fdbaad
Thiago kauai <html5ephp@gmail.com>
2019-06-12 13:53:41 -0300

    Estilizando o Header e o Feed trabalhando o css design do feed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './routes'
 import {BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes/>
     </Router>
   );
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
+import './feed.css'
+import more from '../assets/more.svg'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import send from '../assets/send.svg'
+
 
 export default class Feed extends Component {
     render() {
         return (
-            <div>
-                <h1>Feed</h1>
-            </div>
+          <section id="post-list">
+              <article>
+                  <header>
+                      <div className="user-info">
+                          <span>Thiago kauai</span>
+                          <span className="place">Guarujá</span>
+                      </div>
+					  <img src={more} alt="Mais"/>
+                  </header>
+				  <img src="http://localhost:3001/files/background.jpg" alt="Background"/>
+                  <footer>
+					  <div className="actions">
+						  <img src={like} alt=""/>
+						  <img src={comment} alt=""/>
+						  <img src={send} alt=""/>
+					  </div>
+						  <strong>900 curtidas</strong>
+						  <p>Lorem ipsum dolor sit amet. <span>#react #omnistack #dev</span></p>
+				  </footer>
+			  </article>
+
+              <article>
+                  <header>
+                      <div className="user-info">
+                          <span>Thiago kauai</span>
+                          <span className="place">Guarujá</span>
+                      </div>
+					  <img src={more} alt="Mais"/>
+                  </header>
+				  <img src="http://localhost:3001/files/background.jpg" alt="Background"/>
+                  <footer>
+					  <div className="actions">
+						  <img src={like} alt=""/>
+						  <img src={comment} alt=""/>
+						  <img src={send} alt=""/>
+					  </div>
+						  <strong>900 curtidas</strong>
+						  <p>Lorem ipsum dolor sit amet. <span>#react #omnistack #dev</span></p>
+				  </footer>
+			  </article>
+          </section>
         )
     }
 }
